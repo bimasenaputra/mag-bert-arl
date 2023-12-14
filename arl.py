ad807987eefc01c6b37139a9ddf1b8721bfb2c2e
Bimasena Putra <bimasena.putra@ui.ac.id>
2023-12-14 18:43:59 +0700

    feat: mag bert with ARL model v1

---
+++ arl.py
@@ -0,0 +1,205 @@
+###############################################################################
+# MIT License
+#
+# Copyright (c) 2020 Jardenna Mohazzab, Luc Weytingh, 
+#                    Casper Wortmann, Barbara Brocades Zaalberg
+#
+# This file contains an implementation of the ARL model prented in "Fairness 
+# without Demographics through Adversarially Reweighted Learning" by Lahoti 
+# et al..
+#
+# Author: Jardenna Mohazzab, Luc Weytingh, 
+#         Casper Wortmann, Barbara Brocades Zaalberg 
+# Date Created: 2021-01-01
+###############################################################################
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+class LearnerNN(nn.Module):
+    def __init__(
+        self, hidden_size, num_labels, n_hidden, 
+        activation_fn=nn.ReLU, device='cpu'
+    ):
+        """
+        Implements the learner DNN.
+        Args:
+          embedding_size: list of tuples (n_classes, n_features) containing
+                           embedding sizes for categorical columns.
+          num_labels: number of classes.
+          n_hidden: list of ints, specifies the number of units
+                    in each linear layer.
+          activation_fn: the activation function to use.
+        """
+        super().__init__()
+        self.device = device
+
+        all_layers = []
+        input_size = hidden_size
+
+        for dim in n_hidden:
+            all_layers.append(nn.Linear(input_size, dim))
+            all_layers.append(activation_fn())
+            input_size = dim
+
+        all_layers.append(nn.Linear(n_hidden[-1], num_labels))
+
+        self.layers = nn.Sequential(*all_layers)
+
+
+    def forward(self, features):
+        """
+        The forward step for the learner.
+        """
+
+        # Get the logits output (for calculating loss)
+        logits = self.layers(features)
+        logits.to(self.device)
+
+        return logits
+
+
+class AdversaryNN(nn.Module):
+    def __init__(self, hidden_size, num_labels, n_hidden device='cpu'):
+        """
+        Implements the adversary DNN.
+        Args:
+          embedding_size: list of tuples (n_classes, n_features) containing
+                          embedding sizes for categorical columns.
+          n_num_cols: number of numerical inputs.
+          n_hidden: list of ints, specifies the number of units
+                    in each linear layer.
+        """
+        super().__init__()
+        self.device = device
+
+        all_layers = []
+        input_size = hidden_size
+
+        for dim in n_hidden:
+            all_layers.append(nn.Linear(input_size, dim))
+            input_size = dim
+
+        all_layers.append(nn.Linear(n_hidden[-1], num_labels))
+
+        self.layers = nn.Sequential(*all_layers)
+
+    def forward(self, features):
+        """
+        The forward step for the adversary.
+        """
+        logits = self.layers(features)
+        logits.to(self.device)
+
+        return logits
+
+
+class ARL(nn.Module):
+
+    def __init__(
+        self,
+        hidden_size,
+        num_labels,
+        learner_hidden_units=[64, 32],
+        adversary_hidden_units=[32],
+        batch_size=256,
+        activation_fn=nn.ReLU,
+        device='cpu',
+    ):
+        """
+        Combines the Learner and Adversary into a single module.
+
+        Args:
+          embedding_size: list of tuples (n_classes, embedding_dim) containing
+                    embedding sizes for categorical columns.
+          n_num_cols: the amount of numerical columns in the data.
+          learner_hidden_units: list of ints, specifies the number of units
+                    in each linear layer for the learner.
+          adversary_hidden_units: list of ints, specifies the number of units
+                    in each linear layer for the learner.
+          batch_size: the batch size.
+          activation_fn: the activation function to use for the learner.
+        """
+        super().__init__()
+        torch.autograd.set_detect_anomaly(True)
+
+        self.device = device
+        self.adversary_weights = torch.ones(batch_size, 1)
+
+        self.learner = LearnerNN(
+            hidden_size,
+            num_labels,
+            learner_hidden_units,
+            activation_fn=activation_fn,
+            device=device
+        )
+        self.adversary = AdversaryNN(
+            hidden_size, num_labels, adversary_hidden_units, device=device
+        )
+
+        self.learner.to(device)
+        self.adversary.to(device)
+
+    def forward(self, features):
+        """
+        The forward step for the ARL.
+        """
+        learner_logits = self.learner(features)
+        adversary_logits = self.adversary(features)
+
+        return learner_logits, adversary_logits
+
+    def get_learner_loss(self, logits, targets):
+        """
+        Compute the loss for the learner.
+        """
+        loss = F.binary_cross_entropy_with_logits(logits, targets)
+        loss.to(self.device)
+
+        adversary_weights = self.adversary_weights.to(self.device)
+        weighted_loss = loss * adversary_weights
+        weighted_loss = torch.mean(weighted_loss)
+        return weighted_loss
+
+    def get_adversary_loss(self, logits, targets, adv_logits):
+        """
+        Compute the loss for the adversary.
+        """
+        adversary_weights = self.compute_example_weights(adv_logits)
+        self.adversary_weights = adversary_weights.detach()
+        logits = logits.detach()
+
+        loss = F.binary_cross_entropy_with_logits(logits, targets)
+        loss.to(self.device)
+
+        weighted_loss = -(adversary_weights * loss)
+        weighted_loss = torch.mean(weighted_loss)
+        return weighted_loss
+
+    def compute_example_weights(self, adv_output_layer):
+        if self.num_labels == 1:
+            # Doing regression
+            example_weights = torch.sigmoid(adv_output_layer)
+            mean_example_weights = example_weights.mean()
+            example_weights /= torch.max(mean_example_weights, torch.tensor(1e-4))
+            example_weights = torch.ones_like(example_weights) + example_weights
+
+            return example_weights
+        else:
+            example_weights = torch.softmax(adv_output_layer, dim=1)  
+            mean_example_weights = example_weights.mean(dim=0)  
+            example_weights /= torch.max(mean_example_weights, torch.tensor(1e-4)) 
+            example_weights = torch.ones_like(example_weights) + example_weights 
+            class_weights = example_weights[torch.arange(example_weights.size(0)), labels] 
+
+            return class_weights
+
+    def learner_zero_grad(self):
+        self.learner.zero_grad()
+
+    def adversary_zero_grad(self):
+        self.adversary.zero_grad()
--- bert.py
+++ bert.py
@@ -22,7 +22,7 @@ from transformers.modeling_utils import (
     prune_linear_layer,
 )
 from modeling import MAG
-
+from arl import ARL
 from global_configs import TEXT_DIM, ACOUSTIC_DIM, VISUAL_DIM, DEVICE
 
 logger = logging.getLogger(__name__)
@@ -320,5 +320,93 @@ class MAG_BertForSequenceClassification(BertPreTrainedModel):
                 loss = loss_fct(
                     logits.view(-1, self.num_labels), labels.view(-1))
             outputs = (loss,) + outputs
+
+        return outputs
+
+class MAG_BertWithARL(BertPreTrainedModel):
+    def __init__(self, config, multimodal_config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+
+        self.bert = MAG_BertModel(config, multimodal_config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = ARL(config.hidden_size, config.num_labels)
+
+        self.init_weights()
+
+    def forward(
+        self,
+        input_ids,
+        visual,
+        acoustic,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        pretrain=False
+    ):
+        r"""
+        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size,)`, `optional`, defaults to :obj:`None`):
+            Labels for computing the sequence classification/regression loss.
+            Indices should be in :obj:`[0, ..., config.num_labels - 1]`.
+            If :obj:`config.num_labels == 1` a regression loss is computed (Mean-Square loss),
+            If :obj:`config.num_labels > 1` a classification loss is computed (Cross-Entropy).
+
+    Returns:
+        :obj:`tuple(torch.FloatTensor)` comprising various elements depending on the configuration (:class:`~transformers.BertConfig`) and inputs:
+        loss (:obj:`torch.FloatTensor` of shape :obj:`(1,)`, `optional`, returned when :obj:`label` is provided):
+            Classification (or regression if config.num_labels==1) loss.
+        logits (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, config.num_labels)`):
+            Classification (or regression if config.num_labels==1) scores (before SoftMax).
+        hidden_states (:obj:`tuple(torch.FloatTensor)`, `optional`, returned when ``output_hidden_states=True`` is passed or when ``config.output_hidden_states=True``):
+            Tuple of :obj:`torch.FloatTensor` (one for the output of the embeddings + one for the output of each layer)
+            of shape :obj:`(batch_size, sequence_length, hidden_size)`.
+
+            Hidden-states of the model at the output of each layer plus the initial embedding outputs.
+        attentions (:obj:`tuple(torch.FloatTensor)`, `optional`, returned when ``output_attentions=True`` is passed or when ``config.output_attentions=True``):
+            Tuple of :obj:`torch.FloatTensor` (one for each layer) of shape
+            :obj:`(batch_size, num_heads, sequence_length, sequence_length)`.
+
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
+            heads.
+        """
+
+        outputs = self.bert(
+            input_ids,
+            visual,
+            acoustic,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+        )
+
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
+
+        self.classifier.learner_zero_grad()
+        
+        if pretrain:
+            self.classifer.adversary_zero_grad()
+
+        learner_logits, adversary_logits = self.classifier(pooled_output)
+
+        outputs = (learner_logits,) + outputs[
+            2:
+        ]  # add hidden states and attention if they are here
+
+        if labels is not None:
+            learner_loss = self.classifier.get_learner_loss(logits, labels)
+            adversary_loss = self.classifier.get_adversary_loss(learner_logits, labels, adversary_logits)
+
+            outputs = (learner_loss, adversary_loss,) + outputs
 
         return outputs
