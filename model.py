8c47d1d2ad4f49aa74ee17413160c517ca83874c
Bimasena Putra <bimasena.putra@ui.ac.id>
2023-12-27 16:59:45 +0700

    fix: adversary optimization warmup & training steps

--- model.py
+++ model.py
@@ -119,8 +119,8 @@ class Seq2SeqModel:
         optimizer = AdamW(optimizer_grouped_parameters, lr=self.args.learning_rate)
         scheduler = get_linear_schedule_with_warmup(
             optimizer,
-            num_warmup_steps=self.args.warmup_proportion * (self.args.num_train_optimization_steps - self.args.pretrain_steps),
-            num_training_steps=(self.args.num_train_optimization_steps - self.args.pretrain_steps),
+            num_warmup_steps=self.args.warmup_proportion * (self.args.num_train_optimization_steps - self.args.pretrain_steps * self.args.n_epochs),
+            num_training_steps=(self.args.num_train_optimization_steps - self.args.pretrain_steps * self.args.n_epochs),
         )
 
         # Step 2: Load model if previous training existed
